cd21b2d543a68f0db303a9c25956192b65abd9e8
Isaac Kusi <67994259+Isaac-AAK@users.noreply.github.com>
2023-05-16 23:13:26 -0400

    change data to test

--- src/main/java/com/iaktech/rsvpDashBoard/Controller/Controller.java
+++ src/main/java/com/iaktech/rsvpDashBoard/Controller/Controller.java
@@ -3,6 +3,7 @@ package com.iaktech.rsvpDashBoard.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.iaktech.rsvpDashBoard.repo.RsvpRepo;
 
 @RestController
 @RequestMapping("api/v1/rsvps")
+@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
 	
 	@Autowired
